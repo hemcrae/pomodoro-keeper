e4c6794bc9f3a063bca10901e81eb2004cb4e7fb
hemcrae <halley.mcrae@gmail.com>
2021-04-28 20:52:00 -0400

    added 2 get, 1 patch, 1 delete and 3 post endpoints

--- server/server.js
+++ server/server.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const cors = require('cors');
-const app = express;
+const app = express();
 const PORT = 8080;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/entries', (req, res) => {
+    res.status(200).send('Hello World, this is for entries')
 })
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.get('/entries/:id', (req, res) => {
+    res.status(200).send('This is to GET entires by ID')
+})
+
+app.patch('/entries/:id', (req, res) => {
+    res.status(200).send('This is to EDIT an entry')
+})
+
+app.delete('/entries/:id', (req, res) => {
+    res.status(200).send('This is to DELETE an entry')
+})
+
+app.post('/entries/:id', (req, res) => {
+    res.status(200).send('This is to POST an entry')
+})
+
+app.post('/login', (req, res) => {
+    res.status(200).send('This is to POST a login')
+})
+
+app.post('/register', (req, res) => {
+    res.status(200).send('This is to POST a register')
+})
+
+app.listen(PORT, () => {console.log(`Listening on ${PORT}`)});
