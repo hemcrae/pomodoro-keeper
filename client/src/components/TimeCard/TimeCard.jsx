426610d63e95ec0d2e700831382a0c5cec8afde8
hemcrae <halley.mcrae@gmail.com>
2021-05-09 17:42:09 -0400

    added middlewares to server

--- client/src/components/TimeCard/TimeCard.jsx
+++ client/src/components/TimeCard/TimeCard.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import './TimeCard.scss';
 import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader'
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 
 const TimeCard = () => {
 
     return (
         <>
+        <Card>
+            <CardContent>
+                <CardHeader>
+
+                </CardHeader>
+            </CardContent>
+        </Card>
 
         </>
     ) 
 }
+
+export default TimeCard;
---
+++ server/middlewares/jwtcheck.js
@@ -0,0 +1,11 @@
+const jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://dev-zb2jzg4h.us.auth0.com/.well-known/jwks.json'
+    }),
+    audience: 'http://localhost:8080',
+    issuer: 'https://dev-zb2jzg4h.us.auth0.com/',
+    algorithms: ['RS256']
+    });
