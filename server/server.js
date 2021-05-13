6d97846aa36e9c9114a57b319bed1f0726cf8af3
hemcrae <halley.mcrae@gmail.com>
2021-05-12 22:08:19 -0400

    deleted comments in server.js

--- server/server.js
+++ server/server.js
@@ -19,26 +19,6 @@ app.get('/', (req, res) => {
     res.status(200).send('Pomodoro Keeper API')
 })
 
-// app.get('/entries', (req, res) => {
-//     res.status(200).send('Hello World, this is for entries')
-// })
-
-// app.get('/entries/:id', (req, res) => {
-//     res.status(200).send('This is to GET entires by ID')
-// })
-
-// app.patch('/entries/:id', (req, res) => {
-//     res.status(200).send('This is to EDIT an entry')
-// })
-
-// app.delete('/entries/:id', (req, res) => {
-//     res.status(200).send('This is to DELETE an entry')
-// })
-
-// app.post('/entries/:id', (req, res) => {
-//     res.status(200).send('This is to POST an entry')
-// })
-
 app.post('/login', (req, res) => {
     res.status(200).send('This is to POST a login')
 })
